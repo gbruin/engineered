34b9508d41998a2a56837291cd6fd2ee292e0368
Santoso Wijaya <santa@santambp.local>
2009-09-19 23:37:52 -0700

    Add a Strip model to the database.
    Also add the 'comics' webapp to settings. Create the actual database
    table for 'comics.'

--- comics/models.py
+++ comics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Strip(models.Model):
+
+    # the database fields
+    title    = models.CharField(max_length=256)
+    alt_text = models.CharField(max_length=256)
+    path     = models.CharField(max_length=512)
+    sub_date = models.DateTimeField('submission date')
+
+    def __unicode__(self):
+        return self.title
