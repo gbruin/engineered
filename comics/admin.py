342a33d45772665d90bc073cb9079cd29239592d
Santoso Wijaya <santa@santambp.local>
2009-09-20 17:44:29 -0700

    Use FileField type for Strip model.

--- comics/admin.py
+++ comics/admin.py
@@ -2,7 +2,7 @@ from engineered.comics.models import Strip
 from django.contrib import admin
 
 class StripAdmin(admin.ModelAdmin):
-    fields = ['sub_date', 'title', 'alt_text', 'path']
+    fields = ['sub_date', 'title', 'alt_text', 'strip']
     list_filter = ['sub_date']
     search_fields = ['title']
     date_hierarchy = 'sub_date'
--- comics/models.py
+++ comics/models.py
@@ -5,8 +5,8 @@ class Strip(models.Model):
     # the database fields
     title    = models.CharField(max_length=256)
     alt_text = models.CharField(max_length=256)
-    path     = models.CharField(max_length=512)
     sub_date = models.DateTimeField('submission date')
+    strip    = models.FileField(upload_to='strip')
 
     def __unicode__(self):
         return 'Strip #%s' % self.id
