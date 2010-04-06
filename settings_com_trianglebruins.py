c51eb071d7086ffa9906cd95fe04b7344277d6d9
Santoso Wijaya <santa4nt@boba.dreamhost.com>
2010-04-06 14:32:15 -0700

    Add settings module for engineered.trianglebruins.com.
    Hosted by dreamhost.

---
+++ settings_com_trianglebruins.py
@@ -0,0 +1,87 @@
+# Django settings for engineered project.
+import os.path
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Santoso Wijaya', 'santa.ant@me.com'),
+)
+
+MANAGERS = ADMINS
+EMAIL_SUBJECT_PREFIX = '[ENGINEERED]'
+
+PROJ_DIR = os.path.dirname(__file__)
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'engineered_db'
+DATABASE_USER = 'djtriangle'
+DATABASE_PASSWORD = 'april151907'
+DATABASE_HOST = 'djangodb.trianglebruins.com'
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Los_Angeles'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = '/home/.moonbean/santa4nt/engineered.trianglebruins.com/public/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'http://engineered.trianglebruins.com/public/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/public/media/'
+
+# Make this unique, and don't share it with anybody.
+#SECRET_KEY = 'lq+(3pkhkl*_fbz@!25)7f81hhl%5j_4z-7)6!x)dygp(%h^u&'
+SECRET_KEY = '3_x-u539h6p4$x=ji7u#az^)c!2sj!e7(940f7@ojr3gli+u7i'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'engineered.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJ_DIR, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'engineered.comics'
+)
