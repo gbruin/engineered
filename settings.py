c99f4fb7ae63b5a89a66ff20107ed4f43343998c
Santoso Wijaya <santoso_wijaya@symantec.com>
2010-04-07 13:33:31 -0700

    Bugfix: REQUEST_CONTEXT_PROCESSORS misconfigured.
    Admin pages (after trying to authenticate in) not rendering (template
    errors) because the context processors omitted crucial bindings.
    Introduce the full list of default context processors to fix.

--- settings.py
+++ settings.py
@@ -61,6 +61,9 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
 )
